ed60e52f5127ea0f7d9b1383bb9675ecfc163b25
Kevin Cazelles <kcazelles@omnimed.com>
2022-11-25 14:00:28 -0500

    Shiny v3 (#5)

--- setup.R
+++ setup.R
@@ -21,42 +21,42 @@ resources <- list(
   )
 )
 
-for (resource in resources){
+for (resource in resources) {
   if (!file.exists(resource$outputdir)) dir.create(resource$outputdir)
-  if (!file.exists(paste(resource$outputdir, resource$file, sep = "/"))){
-      download.file(resource$location, destfile = paste(resource$outputdir, resource$file, sep = "/"))
+  if (!file.exists(paste(resource$outputdir, resource$file, sep = "/"))) {
+    download.file(resource$location, destfile = paste(resource$outputdir, resource$file, sep = "/"))
   }
-  if (grepl("zip", resource$file)){
-      utils::unzip(paste(resource$outputdir, resource$file, sep = "/"), exdir = resource$outputdir) 
+  if (grepl("zip", resource$file)) {
+    utils::unzip(paste(resource$outputdir, resource$file, sep = "/"), exdir = resource$outputdir)
   }
-} 
+}
 
 # Import species dictionnary and simplify
 species <- readxl::read_excel(paste(resources$dictionnary$outputdir, resources$dictionnary$file, sep = "/"), "Species-Espèces") |>
-            dplyr::select(Species_ID, English_Name, Scientific_Name, Family_Sci)
+  dplyr::select(Species_ID, English_Name, Scientific_Name, Family_Sci)
 
 # Import density data and simplify
 densities <- readxl::read_excel(paste(resources$data$outputdir, resources$data$file, sep = "/"), "Densities") |>
-              dplyr::select(Group, Month, Stratum, Density) |>
-              dplyr::mutate(
-                Month = replace(Month, Month == "04050607", "April-July"),
-                Month = replace(Month, Month == "08091011", "August-November"),
-                Month = replace(Month, Month == "12010203", "December-March")
-              ) |> 
-              dplyr::filter(Group %in% species$Species_ID)
+  dplyr::select(Group, Month, Stratum, Density) |>
+  dplyr::mutate(
+    Month = replace(Month, Month == "04050607", "April-July"),
+    Month = replace(Month, Month == "08091011", "August-November"),
+    Month = replace(Month, Month == "12010203", "December-March")
+  ) |>
+  dplyr::filter(Group %in% species$Species_ID)
 
 # Import effort data and simplify
 effort <- readxl::read_excel(paste(resources$data$outputdir, resources$data$file, sep = "/"), "Effort") |>
-              dplyr::select(Stratum, Month, nbspecies, nbobs, nbind, nbsamples, nbkm, nbcruiseID, nbdays) |>
-              dplyr::mutate(
-                Month = replace(Month, Month == "04050607", "April-July"),
-                Month = replace(Month, Month == "08091011", "August-November"),
-                Month = replace(Month, Month == "12010203", "December-March")
-              ) |> tidyr::pivot_longer(cols = nbspecies:nbdays) 
+  dplyr::select(Stratum, Month, nbspecies, nbobs, nbind, nbsamples, nbkm, nbcruiseID, nbdays) |>
+  dplyr::mutate(
+    Month = replace(Month, Month == "04050607", "April-July"),
+    Month = replace(Month, Month == "08091011", "August-November"),
+    Month = replace(Month, Month == "12010203", "December-March")
+  ) |>
+  tidyr::pivot_longer(cols = nbspecies:nbdays)
 
 
 # Export as csv
 write.csv(densities, "data/densities.csv", row.names = FALSE)
 write.csv(species, "data/species.csv", row.names = FALSE)
 write.csv(effort, "data/effort.csv", row.names = FALSE)
-
